c60b8741a769e929461954c9edfd9cbbc0fe6997
JDev <joaovictorb.dasilva2003@gmail.com>
2020-12-13 16:10:41 -0400

    cadastro usuarios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const authController = require('./controllers/authController')
+const cors = require('cors')
 const port = 3000
 
 
@@ -8,9 +9,9 @@ const app = express()
 const router = express.Router()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
 
-
-app.listen(port)
+app.listen(process.env.PORT ||  port)
 app.use('/', router);
 app.use('/auth', authController)
 
